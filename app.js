2516fefb5e0d0c6ecf03813a736473fca8ec9f91
Roger K <misterhtmlcss@gmail.com>
2018-02-09 14:41:28 -0700

    Fixed bugs that popped up after I translated project back to my localhost

--- app.js
+++ app.js
@@ -4,48 +4,41 @@ http.onreadystatechange = function() {
 		const response = JSON.parse(http.responseText);
 		const quotes = response.quotes;
 		const quoteLength = quotes.length;
-		const getRandomQuote = max => {
-			return Math.floor(Math.random() * Math.floor(max));
-		};
-
-		const buttonClick = getClick("block");
-		buttonClick.addEventListener("click", sendValue);
+		
+        const buttonClick = getClick("block")
+        
+        buttonClick.addEventListener("click", sendValue)
+        
 		function sendValue() {
-			const randomNumber = getRandomQuote(quoteLength);
+            const randomNumber = getRandomQuote(quoteLength);
 			function buildQuote() {
 				let output = "";
                 output += 
-                `
-                <p class="card-text text-color">
+                `<p class="card-text text-color">
                 ${quotes[randomNumber].content}
-                </p> 
-                <p class="quoteAuthor text-color"> 
-                ${quotes[randomNumber].author}
-                </p>
-                `;
-                return 
-                (document.getElementById("quotes").innerHTML = output)
+                </p><p class="quoteAuthor text-color"> 
+                ${quotes[randomNumber].author}</p>`;
+                return (document.getElementById("quotes").innerHTML = output);
 			}
 			buildQuote(randomNumber);
 			function makeShare() {
 				let output2 = "";
 				output2 +=
                 `
-                <a class="btn btn-primary btn-sm" data-toggle="button" href="https://twitter.com/intent/tweet?hashtags=fcc&text=
-                ${quotes[randomNumber].content} 
-                By ${quotes[randomNumber].author}">tweet me!</a>
+                <a class="btn btn-primary btn-sm" data-toggle="button" href="https://twitter.com/intent/tweet?hashtags=fcc&text=${quotes[randomNumber].content} By ${quotes[randomNumber].author}">tweet me!</a>
                 `
-                return 
-                (document.getElementById("shareable").innerHTML = output2)
+                return (document.getElementById("shareable").innerHTML = output2);
 			}
 			makeShare(randomNumber);
 		}
 	}
 }
-
 http.open("GET", "https://api.myjson.com/bins/13cby5", true);
 http.send();
 
+const getRandomQuote = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 function getClick(id) {
-	return document.getElementById(id);
+    return document.getElementById(id);
 }
